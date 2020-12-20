03b7ca7f89c3d431805e1bfc974abd78d05607ea
Soumyadip Mitra <soumyadip.mitra@gmail.com>
2020-12-20 17:58:29 -0500

    Added Cucumber Html Reports

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# Selenium-Cucumber-Framework
-Automation framework using Cucumber, Java and Selenium
+## Selenium-Cucumber-Framework
+Automation framework using Cucumber, Java and Selenium with Maven
+
+### Prerequisites:
+- Java 8
+- Maven
+
+### Run Tests with default TestRunner options
+> mvn test
+
+### Run Tests for a particular tag
+> mvn test -Dcucumber.options="-t @smoketest"
+
+
+### Reports
+> reports/cucumber-html-reports
+
--- src/test/java/cucumberOptions/TestRunner.java
+++ src/test/java/cucumberOptions/TestRunner.java
@@ -1,9 +1,6 @@
 package cucumberOptions;
 
-
-
 import org.junit.runner.RunWith;
-
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
@@ -13,7 +10,10 @@ import cucumber.api.junit.Cucumber;
 		glue = "stepDefinitions",
 		tags="@regtest",
 		//dryRun=true, //to check if every line has glue code
-		plugin= {"pretty","html:reports","json:reports/cucumber.json","junit:reports/cukes.xml"},
+//		plugin= {"pretty","html:reports","json:reports/cucumber.json","junit:reports/cukes.xml"},
+		// Using reporting-plugin to generate pretty reports
+		// https://www.jvt.me/posts/2019/04/07/prettier-cucumber-jvm-html-reports/
+		plugin = {"pretty", "json:reports/report.json", "de.monochromata.cucumber.report.PrettyReports:reports"},
 		monochrome=true //to make the console output look good
 		//strict = true //this will fail a test case if the glue code is not present
 		)
